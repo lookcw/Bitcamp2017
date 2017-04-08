1774541bb4d9f502ffee7b8462c3862e7c1a1031
ChrisLook <LookWChristopher@gmail.com>
2017-04-08 13:59:32 -0400

    decrease data in fake and real news

--- url_to_article.py
+++ url_to_article.py
@@ -22,16 +22,16 @@ def get_urls(col,input_file,output_file,delim,cate):
 	n=0
 	for line in url_reader:
 		print line[col]
-		if n%1==0:
+		if n%60==0:
 			try:	
 				url = line[col]
 				headlines_writer.writerow([url,cate])
 			except:
 				print "not found"
-		n=(n+1)%2
-#get_urls(1,"url_data/real/newsCorpora.csv","newsCorpora_headlines.csv","\t","real")
+		n=(n+1)%60
+get_urls(1,"url_data/real/newsCorpora.csv","newsCorpora_headlines.csv","\t","real")
 #get_urls(1,"url_data/real/newsCorpora.csv","newsCorpora_headlines.txt","\t")
-get_urls(4,"url_data/fake/fake.csv","fake.csv",",","fake")
+#get_urls(4,"url_data/fake/fake.csv","fake.csv",",","fake")
 
 
 
