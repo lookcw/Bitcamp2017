8da772f4b018f5762b44886f60a59a531bce1c06
ChrisLook <LookWChristopher@gmail.com>
2017-04-08 23:44:33 -0400

    testing sets made

--- url_to_article.py
+++ url_to_article.py
@@ -10,7 +10,7 @@ csv.field_size_limit(sys.maxsize)
 def get_article(url):
 	a = Article(url, language='en')
 	a.download()
-	a.parse()
+	a.parseset	
 	return a.text.replace('\n', '')
 
 
@@ -22,18 +22,18 @@ def get_urls(col,input_file,output_file,delim,cate):
 	n=0
 	for line in url_reader:
 		print line[col]
-		#if n%2==0:
-		try:	
-			url = line[col].replace(',',' ')
-			if url:
-				headlines_writer.writerow([url,cate])
-		except:
-			print "not found"
-		#n=(n+1)%2
-get_urls(1,"url_data/real/newsCorpora.csv","all_newsCorpora_headlines.csv","\t","real")
+		if n%60==1:
+			try:	
+				url = line[col].replace(',',' ')
+				if url:
+					headlines_writer.writerow([url,cate])
+			except:
+				print "not found"
+		n=(n+1)%60
+get_urls(1,"url_data/real/newsCorpora.csv","test_newsCorpora_headlines.csv","\t","real")
 #get_urls(1,"url_data/real/newsCorpora.csv","newsCorpora_headlines.txt","\t")
-get_urls(4,"url_data/fake/fake.csv","all_fake_headlines.csv",",","fake")
-
+#get_urls(4,"url_data/fake/fake.csv","fake_test_set_headlines.csv",",","fake")
+set
 
 
 	
